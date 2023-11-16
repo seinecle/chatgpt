a0fecbabc7cbef120416ab510da87ec0bca017af
seinecle <1244100+seinecle@users.noreply.github.com>
2023-11-16 16:53:20 +0100

    redesign

---
+++ docs/Gruntfile.js
@@ -0,0 +1,8 @@
+/* 
+ * Copyright Clement Levallois 2021-2023. License Attribution 4.0 Intertnational (CC BY 4.0)
+ */
+module.exports = function (grunt) {
+    // Project configuration.
+    grunt.initConfig({
+    });
+};
---
+++ docs/gulpfile.js
@@ -0,0 +1,9 @@
+/* 
+ * Copyright Clement Levallois 2021-2023. License Attribution 4.0 Intertnational (CC BY 4.0)
+ */
+
+var gulp = require('gulp');
+
+gulp.task('default', function () {
+    // place code for your default task here
+});
